78c9358e9f0fbbbdc8f6482e49ae22b2a64e60b8
JEvgeniy01 <evgeniy.turov@mail.ru>
2024-02-26 22:51:23 +0700

    feat: shalloCopy, mergeObjects, removeProperties, compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const target = {};
+  return Object.assign(target, obj);
 }
 
 /**
@@ -32,8 +33,20 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((object) => {
+    Object.entries(object).map((keyValuePair) => {
+      const [key, value] = keyValuePair;
+      if (result[key]) {
+        result[key] += value;
+      } else {
+        result[key] = value;
+      }
+      return null;
+    });
+  });
+  return result;
 }
 
 /**
@@ -49,8 +62,14 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = { ...obj };
+  keys.forEach((key) => {
+    if (result[key]) {
+      delete result[key];
+    }
+  });
+  return result;
 }
 
 /**
@@ -65,8 +84,17 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const arr1 = Object.keys(obj1);
+  const arr2 = Object.keys(obj2);
+  let result = true;
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (obj1[arr1[i]] !== obj2[arr2[i]]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
 }
 
 /**
