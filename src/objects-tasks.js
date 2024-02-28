d4b2fd31589d745a0a4d961635d0111ce84e05b1
JEvgeniy01 <evgeniy.turov@mail.ru>
2024-02-28 19:47:24 +0700

    feat: sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -173,8 +173,19 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let result = true;
+  let counter = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (i !== 0) {
+      counter += queue[i - 1];
+      if (counter < queue[i] - 25) {
+        result = false;
+        break;
+      }
+    }
+  }
+  return result;
 }
 
 /**
